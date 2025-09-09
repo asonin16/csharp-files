f7da112d5614027bcb13591fc18f96e8a89f2d05
Alexey Sonin <asonin16@yahoo.com>
2025-09-09 11:18:29 +0300

    Update TestProject4

--- Learn-MS/TestProject4/Program.cs
+++ Learn-MS/TestProject4/Program.cs
@@ -7,19 +7,24 @@ class Program
         /*
         The following code reverses the srting and calculates the number of 'o'.
         */
-
         string str = "The quick brown fox jumps over the lazy dog.";
-        // convert the message into a char array
+
         char[] charMessage = str.ToCharArray();
-        // Reverse the chars
         Array.Reverse(charMessage);
+
         int x = 0;
-        // count the o's
-        foreach (char i in charMessage) { if (i == 'o') { x++; } }
-        // convert it back to a string
-        string new_message = new String(charMessage);
-        // print it out
+
+        foreach (char i in charMessage)
+        {
+            if (i == 'o')
+            {
+                x++;
+            }
+        }
+        var new_message = new String(charMessage);
+
         Console.WriteLine(new_message);
+        
         Console.WriteLine($"'o' appears {x} times.");
     }
 }
