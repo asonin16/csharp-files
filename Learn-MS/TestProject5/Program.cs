8e2e7600229f200ee006b2a616fdf2abb087ea9a
Alexey Sonin <asonin16@yahoo.com>
2025-09-11 16:32:08 +0300

    Update TestProject5

--- Learn-MS/TestProject5/Program.cs
+++ Learn-MS/TestProject5/Program.cs
@@ -11,5 +11,28 @@ class Program
 
         string myValue = "a";
         Console.WriteLine(myValue == "a");
+
+        string value1 = " a";
+        string value2 = "A ";
+        Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+        Console.WriteLine(String.Equals(value1, " a"));
+
+        Console.WriteLine(1 > 2);
+        Console.WriteLine(1 < 2);
+        Console.WriteLine(1 >= 1);
+        Console.WriteLine(1 <= 1);
+
+        string pangram = "The quick brown fox jumps over the lazy dog.";
+        Console.WriteLine(pangram.Contains("fox"));
+        Console.WriteLine(pangram.Contains("cow"));
+        Console.WriteLine(!pangram.Contains("fox"));
+        Console.WriteLine(!pangram.Contains("cow"));
+
+        int a = 7;
+        int b = 6;
+        Console.WriteLine(a != b); // output: True
+        string s1 = "Hello";
+        string s2 = "Hello";
+        Console.WriteLine(s1 != s2); // output: False
     }
 }
