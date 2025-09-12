e340bbf83e164677f318077eb58c081ff4ee27a6
Alexey Sonin <asonin16@yahoo.com>
2025-09-12 14:19:22 +0300

    Create TestProject6

---
+++ Learn-MS/TestProject6/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TestProject6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int saleAmount = 1001;
+        int discount = saleAmount > 1000 ? 100 : 50;
+        Console.WriteLine($"Discount: {discount}");
+    }
+}
