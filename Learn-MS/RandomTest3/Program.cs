5b7c521a463adcc6a3460f3985d09360ab24427c
Alexey Sonin <asonin16@yahoo.com>
2025-09-12 14:34:48 +0300

    Create RandomTest3

---
+++ Learn-MS/RandomTest3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace RandomTest3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random coin = new Random();
+
+        int flip = coin.Next(1, 11);
+
+        Console.WriteLine("test result: " + (flip > 5 ? "heads" : "tails"));
+    }
+}
