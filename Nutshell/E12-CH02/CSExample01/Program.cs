000581c7375ccc42f910fca340b3e2dc87854826
Alexey Sonin <asonin16@yahoo.com>
2025-09-14 13:14:02 +0300

    Update CSExample01

--- Nutshell/E12-CH02/CSExample01/Program.cs
+++ Nutshell/E12-CH02/CSExample01/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // The instance field Name pertains to an instance of a particular Panda,
+        // whereas Population pertains to the set of all Pandas:
+
+        Panda p1 = new Panda("Pan Dee");
+        Panda p2 = new Panda("Pan Dah");
+        Panda p3 = new Panda("Pan Dou");
+
+        Console.WriteLine(p1.Name);      // Pan Dee
+        Console.WriteLine(p2.Name);      // Pan Dah
+        Console.WriteLine(p3.Name);      // Pan Dou
+
+        Console.WriteLine(Panda.Population);   // 3
+    }
+    public class Panda
+    {
+        public string Name;             // Instance field
+        public static int Population;   // Static field
+        public Panda (string n)         // Constructor
+        {
+            Name = n;                      // Assign the instance field
+            Population = Population + 1;   // Increment the static Population field
+        }
     }
 }
