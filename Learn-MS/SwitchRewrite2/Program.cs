0edb91cf45b9b98ef2a65119091a1125b095d14a
Alexey Sonin <asonin16@yahoo.com>
2025-09-15 12:25:22 +0300

    Update SwitchRewrite2

--- Learn-MS/SwitchRewrite2/Program.cs
+++ Learn-MS/SwitchRewrite2/Program.cs
@@ -14,31 +14,51 @@ class Program
         string color = "";
         string size = "";
 
-        if (product[0] == "01")
-            type = "Sweat shirt";
-        else if (product[0] == "02")
-            type = "T-Shirt";
-        else if (product[0] == "03")
-            type = "Sweat pants";
-        else
-            type = "Other";
+        switch (product[0])
+        {
+            case "01":
+                type = "Sweat shirt";
+                break;
+            case "02":
+                type = "T-Shirt";
+                break;
+            case "03":
+                type = "Sweat pants";
+                break;
+            default:
+                type = "Other";
+                break;
+        }
 
-        if (product[1] == "BL")
-            color = "Black";
-        else if (product[1] == "MN")
-            color = "Maroon";
-        else
-            color = "White";
+        switch (product[1])
+        {
+            case "BL":
+                color = "Black";
+                break;
+            case "MN":
+                color = "Maroon";
+                break;
+            default:
+                color = "White";
+                break;
+        }
 
-        if (product[2] == "S")
-            size = "Small";
-        else if (product[2] == "M")
-            size = "Medium";
-        else if (product[2] == "L")
-            size = "Large";
-        else
-            size = "One Size Fits All";
-
+        switch (product[2])
+        {
+            case "S":
+                size = "Small";
+                break;
+            case "M":
+                size = "Medium";
+                break;
+            case "L":
+                size = "Large";
+                break;
+            default:
+                size = "One size fits all";
+                break;
+        }
+        
         Console.WriteLine($"Product: {size} {color} {type}");
     }
 }
