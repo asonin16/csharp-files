14f7d3b2f22a541b6ec377cc62fcab8877381e61
Alexey Sonin <asonin16@yahoo.com>
2025-09-15 15:52:32 +0300

    Create TestProject8

---
+++ Learn-MS/TestProject8/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TestProject8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string? readResult;
+        // bool validEntry = false;
+        Console.WriteLine("Enter an integer value between 5 and 10:");
+        do
+        {
+            readResult = Console.ReadLine();
+            if (readResult != null)
+            {
+                int numericValue = 0;
+
+                bool validNumber = int.TryParse(readResult, out numericValue);
+
+                if (validNumber == false)
+                {
+                    Console.WriteLine("Sorry, you entered an invalid number, please try again");
+                    continue;
+                }
+
+                if (numericValue < 5 || numericValue > 10)
+                {
+                    Console.WriteLine($"You entered {numericValue}. Please enter a number between 5 and 10.");
+                }
+                else
+                {
+                    Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
+                    // validEntry = true;
+                    break;
+                }
+            }
+        } while (true);
+    }
+}
