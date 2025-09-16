20548e6ba446b9d67d71b8eb1fc7d0a359e7fbdc
Alexey Sonin <asonin16@yahoo.com>
2025-09-16 11:25:45 +0300

    Update PetsProject

--- Learn-MS/PetsProject/Program.cs
+++ Learn-MS/PetsProject/Program.cs
@@ -110,7 +110,17 @@ class Program
             {
                 case "1":
                     // List all of our current pet information
-                    Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+                    for (int i = 0; i < maxPets; i++)
+                    {
+                        if (ourAnimals[i, 0] != "ID #: ")
+                        {
+                            Console.WriteLine();
+                            for (int j = 0; j < 6; j++)
+                            {
+                                Console.WriteLine(ourAnimals[i, j]);
+                            }
+                        }
+                    }
                     Console.WriteLine("Press the Enter key to continue.");
                     readResult = Console.ReadLine();
                     break;
@@ -158,7 +168,8 @@ class Program
                     break;
                 default:
                     break;
-            }  
+            }
+            
         } while (menuSelection != "exit");
     }
 }
