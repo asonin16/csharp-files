57bc7ac964273b03f72abc2b76dce79acaf21f81
Alexey Sonin <asonin16@yahoo.com>
2025-09-16 10:17:38 +0300

    Create SwitchRewrite4

---
+++ Learn-MS/SwitchRewrite4/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SwitchRewrite4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // SKU = Stock Keeping Unit. 
+        // SKU value format: <product #>-<2-letter color code>-<size code>
+        string sku = "01-MN-L";
+        string[] product = sku.Split('-');
+        
+        string type = product[0] switch
+        {
+            "01" => "Sweat shirt",
+            "02" => "T-Shirt",
+            "03" => "Sweat pants",
+            _ => "Other",
+        };
+
+        string color = product[1] switch
+        {
+            "BL" => "Black",
+            "MN" => "Maroon",
+            _ => "White",
+        };
+
+        string size = product[2] switch
+        {
+            "S" => "Small",
+            "M" => "Medium",
+            "L" => "Large",
+            _ => "One size fits all",
+        };
+        Console.WriteLine($"Product: {size} {color} {type}");
+    }
+}
