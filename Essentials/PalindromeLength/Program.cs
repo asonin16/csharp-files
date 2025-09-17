853efb371a5f3e464fc882d3050fccc3ef934ab8
Alexey Sonin <asonin16@yahoo.com>
2025-09-17 16:30:57 +0300

    Create PalindromeLength

---
+++ Essentials/PalindromeLength/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PalindromeLength;
+
+public class Program
+{
+    public static void Main()
+    {
+        string? palindrome;
+
+        Console.Write("Enter a palindrome: ");
+        palindrome = Console.ReadLine();
+
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+        Console.WriteLine($"The palindrome \"{palindrome}\" is {palindrome.Length} characters.");
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+    }
+}
