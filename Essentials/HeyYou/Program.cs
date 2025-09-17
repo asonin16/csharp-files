194fd3cb1da166c4cb2445fc413c56c522356f6b
Alexey Sonin <asonin16@yahoo.com>
2025-09-17 11:54:49 +0300

    Create HeyYou

---
+++ Essentials/HeyYou/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HeyYou;
+
+public class Program
+{
+    public static void Main()
+    {
+        string? firstName;
+        string? lastName;
+
+        Console.WriteLine("Hey you!");
+
+        Console.Write("Enter your first name: ");
+        firstName = Console.ReadLine();
+
+        Console.Write("Enter your last name: ");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your full name is {firstName} {lastName}.");
+    }
+}
