3357bc95d762359654a4688f9c6ebb7385bc1b61
Alexey Sonin <asonin16@yahoo.com>
2025-09-17 17:28:08 +0300

    Create TryParseTest

---
+++ Essentials/TryParseTest/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TryParseTest;
+
+#region INCLUDE
+public class Program
+{
+    public static void Main()
+    {
+        string? input;
+
+        Console.Write("Enter a number: ");
+        input = Console.ReadLine();
+
+        if (double.TryParse(input, out double number))
+        {
+            Console.WriteLine($"'input' was parsed successfully to {number}");
+        }
+        else
+        {
+            Console.WriteLine("The text entered was not a valid number.");
+        }
+
+        Console.WriteLine($"'number' currently has the value: {number}");
+    }
+}
+#endregion INCLUDE
