2b120f47c7aad0658a14f89a40f544b599a9c9c8
Alexey Sonin <asonin16@yahoo.com>
2025-09-17 11:11:51 +0300

    Create HelloWorld

---
+++ Essentials/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld;
+
+#region INCLUDE
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello.  My name is Inigo Montoya.");
+    }
+}
+#endregion INCLUDE
