105f81411e73e9c708ef1e6795af5163684a84e8
Alexey Sonin <asonin16@yahoo.com>
2025-09-18 14:00:56 +0300

    Create CheckingForNull

---
+++ Essentials/CheckingForNull/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CheckingForNull;
+
+public class Program
+{
+    #region INCLUDE
+    public static void Main(string[] args)
+    {
+        int? number = null;
+
+        #region EXCLUDE
+        if (args.Length > 0)
+        {
+            number = args[0].Length;
+        }
+        #endregion EXCLUDE
+
+        if (number is null)
+        {
+            Console.WriteLine("'number' requires a value and cannot be null");
+        }
+        else
+        {
+            Console.WriteLine($"'number' doubled is {number * 2}.");
+        }
+    }
+    #endregion INCLUDE
+}
