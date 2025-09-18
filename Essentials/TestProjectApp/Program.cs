6bef08b3a3b12dd0233f5d8af724565a58c0daad
Alexey Sonin <asonin16@yahoo.com>
2025-09-18 17:57:00 +0300

    Create TestProjectApp

---
+++ Essentials/TestProjectApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TestProjectApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var (name, capital, population, gdpPerCapita) = ("Russia", "Moscow", 142_000_000.00M, 13000.00M);
+        Console.WriteLine($"Country: {name},\nCapital: {capital},\nPopulation: {population},\nGDP per capita: {gdpPerCapita}.");
+        char[] ch = ['A', 'B', 'C', 'D', 'E', 'F'];
+        Console.WriteLine(string.Join("", ch[..].Reverse()));
+    }
+}
