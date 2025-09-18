42d0054ad5db5802937d5a3baeac2a7b75b8c128
Alexey Sonin <asonin16@yahoo.com>
2025-09-18 19:24:55 +0300

    Update TestProjectApp

--- Essentials/TestProjectApp/Program.cs
+++ Essentials/TestProjectApp/Program.cs
@@ -6,7 +6,9 @@ class Program
     {
         var (name, capital, population, gdpPerCapita) = ("Russia", "Moscow", 142_000_000.00M, 13000.00M);
         Console.WriteLine($"Country: {name},\nCapital: {capital},\nPopulation: {population},\nGDP per capita: {gdpPerCapita}.");
+        
         char[] ch = ['A', 'B', 'C', 'D', 'E', 'F'];
+        Console.WriteLine(new string(ch[..]));
         Console.WriteLine(string.Join("", ch[..].Reverse()));
     }
 }
