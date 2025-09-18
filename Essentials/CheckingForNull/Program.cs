7c73ee301fa9b3e1ce8dc0ce1e2cd390b76d6ef9
Alexey Sonin <asonin16@yahoo.com>
2025-09-18 14:22:49 +0300

    Update CheckingForNull

--- Essentials/CheckingForNull/Program.cs
+++ Essentials/CheckingForNull/Program.cs
@@ -7,33 +7,24 @@ public class Program
     {
         int? number = null;
 
-        string? text = "simple sentence";
+        string? text = null;
+        // string? text = "sample string";
         int? length = text?.Length;
 
         if (length is null)
-        {
             Console.WriteLine("text was not created");
-        }
         else
-        {
             Console.WriteLine($"'text length' is {length}");
-        }
 
         #region EXCLUDE
         if (args.Length > 0)
-        {
             number = args[0].Length;
-        }
         #endregion EXCLUDE
 
         if (number is null)
-        {
             Console.WriteLine("'number' requires a value and cannot be null");
-        }
         else
-        {
             Console.WriteLine($"'number' doubled is {number * 2}.");
-        }
     }
     #endregion INCLUDE
 }
