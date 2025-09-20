8467c396611674bf3e05583c81a56a8ebf808b17
Alexey Sonin <asonin16@yahoo.com>
2025-09-20 17:32:16 +0300

    Create AnotherTest4

---
+++ Learn-MS/AnotherTest4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AnotherTest4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string value = "102";
+        if (int.TryParse(value, out int result))
+        {
+            Console.WriteLine($"Measurement: {result}");
+        }
+        else
+        {
+            Console.WriteLine("Unable to report the measurement.");
+        }
+        Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+    }
+}
