5bd7997f1b4d33aa378c45fc977135a353daa0b0
Alexey Sonin <asonin16@yahoo.com>
2025-09-20 14:11:41 +0300

    Create AnotherTest2

---
+++ Learn-MS/AnotherTest2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AnotherTest2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Signed integral types:");
+
+        Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+        Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+        Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+        Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+    }
+}
