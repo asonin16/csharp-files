323dabe195d7db8bea1d2dd5eee604b898d0e3ab
Alexey Sonin <asonin16@yahoo.com>
2025-09-20 15:36:51 +0300

    Create AnotherTest3

---
+++ Learn-MS/AnotherTest3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AnotherTest3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int first = 2;
+        string second = "4";
+        string result = first + second;
+        Console.WriteLine(result);
+    }
+}
