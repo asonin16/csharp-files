b5f355b98745c71689c5a280a0a8f1f6f86205a0
Alexey Sonin <asonin16@yahoo.com>
2025-09-20 17:53:04 +0300

    Create AnotherTest5

---
+++ Learn-MS/AnotherTest5/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace AnotherTest5;
+
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+        string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+        string message = "";
+        float total = 0;
+
+        foreach (var value in values)
+        {
+            if (float.TryParse(value, out float result))
+            {
+                total += result;
+            }
+            else
+            {
+                message += value;
+            }
+        }
+
+        Console.WriteLine($"Message: {message}");
+        Console.WriteLine($"Total: {total}");
+    }
+}
