97643e381d9f9ad251f2dd1fbda15af05e9d9826
Alexey Sonin <asonin16@yahoo.com>
2025-09-24 14:03:02 +0300

    Update AnotherTest8

--- Learn-MS/AnotherTest8/Program.cs
+++ Learn-MS/AnotherTest8/Program.cs
@@ -7,7 +7,14 @@ class Program
         string value = "abc123";
         char[] valueArray = value.ToCharArray();
         Array.Reverse(valueArray);
-        string result = new string(valueArray);
+        // string result = new string(valueArray);
+        string result = String.Join(",", valueArray);
         Console.WriteLine(result);
+
+        string[] items = result.Split(',');
+        foreach (string item in items)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
