e6d950d56fbab106f855af45dcbf97f590fbdecd
Alexey Sonin <asonin16@yahoo.com>
2025-09-24 11:13:41 +0300

    Update AnotherTest7

--- Learn-MS/AnotherTest7/Program.cs
+++ Learn-MS/AnotherTest7/Program.cs
@@ -8,6 +8,7 @@ class Program
 
         Console.WriteLine("Sorted...");
         Array.Sort(pallets);
+        
         foreach (var pallet in pallets)
         {
             Console.WriteLine($"-- {pallet}");
@@ -16,13 +17,13 @@ class Program
         Console.WriteLine("");
         Console.WriteLine("Reversed...");
         Array.Reverse(pallets);
+
         foreach (var pallet in pallets)
         {
             Console.WriteLine($"-- {pallet}");
         }
 
         Console.WriteLine("");
-
         Console.WriteLine($"Before: {pallets[0].ToLower()}");
         Array.Clear(pallets, 0, 2);
 
@@ -32,17 +33,27 @@ class Program
         }
 
         Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+
         foreach (var pallet in pallets)
         {
             Console.WriteLine($"-- {pallet}");
         }
-        
+
         Console.WriteLine("");
         Array.Resize(ref pallets, 6);
         Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
 
         pallets[4] = "C01";
         pallets[5] = "C02";
+
+        foreach (var pallet in pallets)
+        {
+            Console.WriteLine($"-- {pallet}");
+        }
+        
+        Console.WriteLine("");
+        Array.Resize(ref pallets, 3);
+        Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
 
         foreach (var pallet in pallets)
         {
