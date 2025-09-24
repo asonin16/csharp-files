15aec702316333058024c91f77fb8bc9147d3d04
Alexey Sonin <asonin16@yahoo.com>
2025-09-24 13:21:25 +0300

    Create AnotherTest8

---
+++ Learn-MS/AnotherTest8/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AnotherTest8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string value = "abc123";
+        char[] valueArray = value.ToCharArray();
+        Array.Reverse(valueArray);
+        string result = new string(valueArray);
+        Console.WriteLine(result);
+    }
+}
