b4a3137f69126344695a5be26cc18a5839d0242a
Alexey Sonin <asonin16@yahoo.com>
2025-09-24 15:09:15 +0300

    Create OrderIDChecks

---
+++ Learn-MS/OrderIDChecks/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace OrderIDChecks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+        string[] ordersIDs = orderStream.Split(',');
+        Array.Sort(ordersIDs);
+
+        foreach (var orderID in ordersIDs)
+        {
+            string message = orderID;
+            if (orderID.Length != 4)
+            {
+                message += "\t- Error";
+            }
+            Console.WriteLine(message);
+        }
+    }
+}
