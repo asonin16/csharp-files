b1417a45cff0e43a3bf8327f39cc221be39d3d56
Alexey Sonin <asonin16@yahoo.com>
2025-09-25 16:49:51 +0300

    Create StringFormatting5

---
+++ Learn-MS/StringFormatting5/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StringFormatting5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string message = "Find what is (inside the parentheses)";
+
+        int openingPosition = message.IndexOf('(');
+        int closingPosition = message.IndexOf(')');
+
+        openingPosition += 1;
+
+        // Console.WriteLine(openingPosition);
+        // Console.WriteLine(closingPosition);
+
+        int length = closingPosition - openingPosition;
+        Console.WriteLine(message.Substring(openingPosition, length));
+    }
+}
