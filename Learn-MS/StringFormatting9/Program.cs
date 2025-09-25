db0ecf83119819bd4a04f51af7bb96cd2bc183d7
Alexey Sonin <asonin16@yahoo.com>
2025-09-25 20:06:49 +0300

    Create StringFormatting9

---
+++ Learn-MS/StringFormatting9/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace StringFormatting9;
+
+class Program
+{
+    static void Main()
+    {
+        const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+        string quantity = "";
+        string output = "";
+
+        quantity += input;
+        quantity = quantity.Replace("<span>", "=");
+        quantity = quantity.Replace("</span>", "=");
+        int openingPosition = quantity.IndexOf('=');
+        int closingPosition = quantity.LastIndexOf('=');
+        openingPosition += 1;
+        int length = closingPosition - openingPosition; 
+        quantity = quantity.Substring(openingPosition, length);
+
+        output += input;
+        output = output.Replace("</div>", "");
+        output = output.Replace("<div>", "");
+        output = output.Replace("&trade", "&reg");
+
+        Console.WriteLine($"Quantity: {quantity}");
+        Console.WriteLine($"Output: {output}");
+    }
+}
