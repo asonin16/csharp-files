d868fa2ceac035b47e28bbe97356a76c54ffcb04
Alexey Sonin <asonin16@yahoo.com>
2025-09-25 16:56:52 +0300

    Update StringFormatting5

--- Learn-MS/StringFormatting5/Program.cs
+++ Learn-MS/StringFormatting5/Program.cs
@@ -4,17 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        string message = "Find what is (inside the parentheses)";
+        string message1 = "Find what is (inside the parentheses)";
 
-        int openingPosition = message.IndexOf('(');
-        int closingPosition = message.IndexOf(')');
+        int openingPosition1 = message1.IndexOf('(');
+        int closingPosition1 = message1.IndexOf(')');
 
-        openingPosition += 1;
+        openingPosition1 += 1;
 
         // Console.WriteLine(openingPosition);
         // Console.WriteLine(closingPosition);
 
-        int length = closingPosition - openingPosition;
-        Console.WriteLine(message.Substring(openingPosition, length));
+        int length1 = closingPosition1 - openingPosition1;
+        Console.WriteLine(message1.Substring(openingPosition1, length1));
+
+        string message = "What is the value <span>between the tags</span>?";
+
+        const string openSpan = "<span>";
+        const string closeSpan = "</span>";
+
+        int openingPosition2 = message.IndexOf(openSpan);
+        int closingPosition2 = message.IndexOf(closeSpan);
+
+        openingPosition2 += openSpan.Length;
+        int length2 = closingPosition2 - openingPosition2;
+        Console.WriteLine(message.Substring(openingPosition2, length2));
     }
 }
