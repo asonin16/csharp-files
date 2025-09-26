bca94c12cffe0da082adabb68e6124cd905fd8e6
Alexey Sonin <asonin16@yahoo.com>
2025-09-26 12:21:38 +0300

    Update PetsProject3

--- Learn-MS/PetsProject3/Program.cs
+++ Learn-MS/PetsProject3/Program.cs
@@ -88,7 +88,6 @@ class Program
             ourAnimals[i, 3] = "Nickname: " + animalNickname;
             ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
             ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
-            // ourAnimals[i, 6] = "Suggested Donation: " + suggestedDonation;
 
             if (!decimal.TryParse(suggestedDonation, out decimalDonation))
             {
@@ -137,7 +136,43 @@ class Program
 
                 case "2":
                     // Display all dogs with a specified characteristic
-                    Console.WriteLine("\nUNDER CONSTRUCTION - please check back next month to see progress.");
+                    string dogCharacteristic = "";
+
+                    while (dogCharacteristic == "")
+                    {
+                        // have the user enter physical characteristics to search for
+                        Console.WriteLine($"\nEnter one desired dog characteristics to search for");
+                        readResult = Console.ReadLine();
+                        if (readResult != null)
+                        {
+                            dogCharacteristic = readResult.ToLower().Trim();
+                        }
+                    }
+                    
+                    string dogDescription = "";
+                    bool noMatchesDog = true;
+
+                    // #6 loop through the ourAnimals array to search for matching animals
+                    for (int i = 0; i < maxPets; i++)
+                    {
+                        if (ourAnimals[i, 1].Contains("dog"))
+                        {
+                            // #7 Search combined descriptions and report results
+                            dogDescription = ourAnimals[i, 4] + "\n" + ourAnimals[i, 5];
+                            if (dogDescription.Contains(dogCharacteristic))
+                            {
+                                Console.WriteLine($"\nOur dog {ourAnimals[i, 3]} is a match!");
+                                Console.WriteLine(dogDescription);
+                                noMatchesDog = false;
+                            }
+                        }
+                    }
+
+                    if (noMatchesDog)
+                    {
+                        Console.WriteLine("None of our dogs are a match found for: " + dogCharacteristic);
+                    }
+
                     Console.WriteLine("Press the Enter key to continue.");
                     readResult = Console.ReadLine();
                     break;
