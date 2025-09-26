7a9af15ab9c2dcaa31e3a269a27eb129fcf559b6
Alexey Sonin <asonin16@yahoo.com>
2025-09-26 11:01:21 +0300

    Update PetsProject3

--- Learn-MS/PetsProject3/Program.cs
+++ Learn-MS/PetsProject3/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace PetsProject3;
+﻿using System.Globalization;
+
+namespace PetsProject3;
 
 class Program
 {
     static void Main()
     {
+        CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
         // #1 the ourAnimals array will store the following: 
         string animalSpecies = "";
         string animalID = "";
@@ -11,14 +15,16 @@ class Program
         string animalPhysicalDescription = "";
         string animalPersonalityDescription = "";
         string animalNickname = "";
+        string suggestedDonation = "";
 
         // #2 variables that support data entry
         int maxPets = 8;
         string? readResult;
         string menuSelection = "";
+        decimal decimalDonation = 0.00m;
 
         // #3 array used to store runtime data, there is no persisted data
-        string[,] ourAnimals = new string[maxPets, 6];
+        string[,] ourAnimals = new string[maxPets, 7];
 
         // #4 create sample data ourAnimals array entries
         for (int i = 0; i < maxPets; i++)
@@ -32,6 +38,7 @@ class Program
                     animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 45 pounds. housebroken.";
                     animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
                     animalNickname = "lola";
+                    suggestedDonation = "85.00";
                     break;
 
                 case 1:
@@ -41,8 +48,9 @@ class Program
                     animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
                     animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
                     animalNickname = "gus";
+                    suggestedDonation = "49.99";
                     break;
-                
+
                 case 2:
                     animalSpecies = "cat";
                     animalID = "c3";
@@ -50,6 +58,7 @@ class Program
                     animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
                     animalPersonalityDescription = "friendly";
                     animalNickname = "snow";
+                    suggestedDonation = "40.00";
                     break;
 
                 case 3:
@@ -59,8 +68,9 @@ class Program
                     animalPhysicalDescription = "Medium sized, long hair, yellow, female, about 10 pounds. Uses litter box.";
                     animalPersonalityDescription = "A people loving cat that likes to sit on your lap.";
                     animalNickname = "Lion";
+                    suggestedDonation = "";
                     break;
-                
+
                 default:
                     animalSpecies = "";
                     animalID = "";
@@ -68,6 +78,7 @@ class Program
                     animalPhysicalDescription = "";
                     animalPersonalityDescription = "";
                     animalNickname = "";
+                    suggestedDonation = "";
                     break;
             }
 
@@ -77,7 +88,13 @@ class Program
             ourAnimals[i, 3] = "Nickname: " + animalNickname;
             ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
             ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
-            
+            // ourAnimals[i, 6] = "Suggested Donation: " + suggestedDonation;
+
+            if (!decimal.TryParse(suggestedDonation, out decimalDonation))
+            {
+                decimalDonation = 45.00m; // if suggestedDonation NOT a number, default to 45.00
+            }
+            ourAnimals[i, 6] = $"Suggested Donation: {decimalDonation:C2}";
         }
 
         // #5 display the top-level menu options
@@ -108,7 +125,7 @@ class Program
                         if (ourAnimals[i, 0] != "ID #: ")
                         {
                             Console.WriteLine();
-                            for (int j = 0; j < 6; j++)
+                            for (int j = 0; j < 7; j++) // increased exit condition
                             {
                                 Console.WriteLine(ourAnimals[i, j]);
                             }
