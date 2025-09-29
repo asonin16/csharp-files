060b85f9b8f9d48873566f5beb1d47713cafe2f2
Alexey Sonin <asonin16@yahoo.com>
2025-09-29 16:15:42 +0300

    Create TestMethods2

---
+++ Learn-MS/TestMethods2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TestMethods2;
+
+class Program
+{
+    static void Main()
+    {
+        string[] students = { "Jenna", "Ayesha", "Carlos", "Viktor" };
+
+        DisplayStudents(students);
+        DisplayStudents(new string[] { "Robert", "Vanya" });
+    }
+
+    static void DisplayStudents(string[] students)
+    {
+        foreach (string student in students)
+        {
+            Console.Write($"{student}, ");
+        }
+        Console.WriteLine();
+    }
+}
