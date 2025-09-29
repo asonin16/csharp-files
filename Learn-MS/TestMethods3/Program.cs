162034884a5d8b5baf6aaf9cbd0f8ab358883096
Alexey Sonin <asonin16@yahoo.com>
2025-09-29 16:20:16 +0300

    Create TestMethods3

---
+++ Learn-MS/TestMethods3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TestMethods3;
+
+class Program
+{
+    const double pi = 3.14159;
+    static void Main(string[] args)
+    {
+        PrintCircleInfo(12);
+        PrintCircleInfo(24);
+            }
+
+    static void PrintCircleInfo(int radius)
+    {
+        Console.WriteLine($"Circle with radius {radius}");
+        PrintCircleArea(radius);
+        PrintCircleCircumference(radius);
+    }
+
+    static void PrintCircleArea(int radius)
+    {
+        double area = pi * (radius * radius);
+        Console.WriteLine($"Area = {area}");
+    }
+
+    static void PrintCircleCircumference(int radius)
+    {
+        double circumference = 2 * pi * radius;
+        Console.WriteLine($"Circumference = {circumference}");
+    }
+}
