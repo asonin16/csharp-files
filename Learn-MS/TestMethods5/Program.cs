2e4e48bf2075bcf0cdeef41fdaf54d7588e1ff04
Alexey Sonin <asonin16@yahoo.com>
2025-09-29 18:17:25 +0300

    Create TestMethods5

---
+++ Learn-MS/TestMethods5/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace TestMethods5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string status1 = "Healthy";
+
+        Console.WriteLine($"Start: {status1}");
+        SetHealth1(status1, false);
+        Console.WriteLine($"End: {status1}");
+
+        string status2 = "Healthy";
+
+        Console.WriteLine($"Start: {status2}");
+        SetHealth2(ref status2, false);
+        Console.WriteLine($"End: {status2}");
+
+    }
+
+    static void SetHealth1(string status, bool isHealthy)
+    {
+        status = (isHealthy ? "Healthy" : "Unhealthy");
+        Console.WriteLine($"Middle: {status}");
+    }
+
+    static void SetHealth2(ref string status, bool isHealthy) 
+    {
+        status = (isHealthy ? "Healthy" : "Unhealthy");
+        Console.WriteLine($"Middle: {status}");
+    }
+}
