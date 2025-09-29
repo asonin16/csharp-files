18c26c24b269ef887270abe6d44269503bfc5fb8
Alexey Sonin <asonin16@yahoo.com>
2025-09-29 18:09:12 +0300

    Create TestMethods4

---
+++ Learn-MS/TestMethods4/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace TestMethods4;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 3;
+        int b = 4;
+        int c = 0;
+
+        Multiply(a, b, c);
+        Console.WriteLine($"global statement: {a} x {b} = {c}");
+
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        PrintArray(array);
+        ClearArray(array);
+        PrintArray(array);
+    }
+
+    static void Multiply(int a, int b, int c)
+    {
+        c = a * b;
+        Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (int a in array)
+        {
+            Console.Write($"{a} ");
+        }
+        Console.WriteLine();
+    }
+
+    static void ClearArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = 0;
+        }
+    }
+}
