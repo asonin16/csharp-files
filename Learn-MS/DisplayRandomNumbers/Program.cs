b480ad46d0d7f32cd919ab7635da33264eba4387
Alexey Sonin <asonin16@yahoo.com>
2025-09-30 12:54:43 +0300

    Create DisplayRandomNumbers

---
+++ Learn-MS/DisplayRandomNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DisplayRandomNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Generating random numbers:");
+        DisplayRandomNumbers();
+
+        static void DisplayRandomNumbers() 
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.Write($"{random.Next(1, 100)} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
