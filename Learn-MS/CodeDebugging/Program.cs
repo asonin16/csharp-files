ed06997d7ee94c35c7901c4af146e34a86a1e7a5
Alexey Sonin <asonin16@yahoo.com>
2025-10-01 17:15:30 +0300

    Create CodeDebugging

---
+++ Learn-MS/CodeDebugging/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeDebugging;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var students = new string[] {"Sophia", "Nicolas", "Zahirah", "Jeong"};
+
+        int studentCount = students.Length;
+
+        Console.WriteLine("The final name is: " + students[studentCount - 1]);
+    }
+}
