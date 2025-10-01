21a38326c747ee162d4db5ee4abc31fd4f19d6a2
Alexey Sonin <asonin16@yahoo.com>
2025-10-01 11:06:04 +0300

    Create PettingZooTest

---
+++ Learn-MS/PettingZooTest/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace PettingZooTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] pettingZoo =
+        {
+            "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+            "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+            "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+        };
+
+        RandomizeAnimals();
+        string[,] group = AssignGroup();
+        Console.WriteLine("School A");
+        PrintGroup(group);
+
+        void RandomizeAnimals()
+        {
+            var random = new Random();
+
+            for (int i = 0; i < pettingZoo.Length; i++)
+            {
+                int r = random.Next(pettingZoo.Length);
+
+                string temp = pettingZoo[i];
+                pettingZoo[i] = pettingZoo[r];
+                pettingZoo[r] = temp;
+            }
+        }
+
+        string[,] AssignGroup(int groups = 6)
+        {
+            string[,] result = new string[groups, pettingZoo.Length / groups];
+            int start = 0;
+
+            for (int i = 0; i < groups; i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    result[i, j] = pettingZoo[start++];
+                }
+            }
+
+            return result;
+        }
+
+        void PrintGroup(string[,] group) 
+        {
+            for (int i = 0; i < group.GetLength(0); i++) 
+            {
+                Console.Write($"Group {i + 1}: ");
+                for (int j = 0; j < group.GetLength(1); j++) 
+                {
+                    Console.Write($"{group[i,j]}  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
