1fda0347b63b6c97caf89340e7d9305176874359
Alexey Sonin <asonin16@yahoo.com>
2025-10-02 18:36:50 +0300

    Create Debug105X

---
+++ SProjects/Debug105X/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Debug105X;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*  
+        This code instantiates a value and then calls the ChangeValue method
+        to update the value. The code then prints the updated value to the console.
+        */
+        int x = 5;
+
+        ChangeValue(ref x);
+
+        Console.WriteLine(x);
+
+        void ChangeValue(ref int value) 
+        {
+            value = 10;
+        }
+    }
+}
