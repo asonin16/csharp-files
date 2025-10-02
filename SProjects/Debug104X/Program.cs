d2263a345b0612bd6167397cbd7048b9a83630aa
Alexey Sonin <asonin16@yahoo.com>
2025-10-02 17:29:25 +0300

    Create Debug104X

---
+++ SProjects/Debug104X/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Debug104X;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool exit = false;
+        var rand = new Random();
+        int num1 = 5;
+        int num2 = 5;
+
+        do
+        {
+            num1 = rand.Next(1, 11);
+            num2 = num1 + rand.Next(1, 51);
+
+        } while (exit == false);
+    }
+}
