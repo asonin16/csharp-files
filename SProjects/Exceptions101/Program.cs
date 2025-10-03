ad208048419c32f9cef3182b55557980a0b85192
Alexey Sonin <asonin16@yahoo.com>
2025-10-03 12:16:45 +0300

    Create Exceptions101

---
+++ SProjects/Exceptions101/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exceptions101;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double float1 = 3000.0;
+        double float2 = 0.0;
+        int number1 = 3000;
+        int number2 = 0;
+
+        Console.WriteLine(float1 / float2);
+        Console.WriteLine(number1 / number2);
+        Console.WriteLine("Exit program");
+    }
+}
