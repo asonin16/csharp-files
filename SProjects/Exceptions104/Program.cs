f43fdf8b2f3be2fa679d3b9b8782f342b7fde051
Alexey Sonin <asonin16@yahoo.com>
2025-10-03 14:29:34 +0300

    Create Exceptions104

---
+++ SProjects/Exceptions104/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exceptions104;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // inputValues is used to store numeric values entered by a user
+        var inputValues = new string[] {"three", "9999999999", "0", "2" };
+
+        foreach (string inputValue in inputValues)
+        {
+            int numValue = 0;
+            try
+            {
+                numValue = int.Parse(inputValue);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid readResult. Please enter a valid number.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The number you entered is too large or too small.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
