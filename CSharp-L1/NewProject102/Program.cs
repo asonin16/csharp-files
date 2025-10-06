e20fa8ccb9d8db6962804fa1fcc5cdbf823be0c8
Alexey Sonin <asonin16@yahoo.com>
2025-10-06 15:15:54 +0300

    Create NewProject102

---
+++ CSharp-L1/NewProject102/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace NewProject102;
+/// <summary>
+/// Main program
+/// </summary>
+internal class Program
+{
+    /// <summary>
+    /// Entry point
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Brave New World!");
+    }
+}
