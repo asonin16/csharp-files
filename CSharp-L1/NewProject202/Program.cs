fd5d0995bdd706bb010fa563e5e4cff57fe2c214
Alexey Sonin <asonin16@yahoo.com>
2025-10-07 12:43:37 +0300

    Create NewProject202

---
+++ CSharp-L1/NewProject202/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace NewProject202;
+
+record struct Coords3D(int X, int Y, int Z);
+
+record class Point3D(int X, int Y, int Z, string Color);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var c1 = new Coords3D(1, 2, 3);
+        var c2 = new Coords3D(1, 2, 3);
+        var c3 = c1;
+        Console.WriteLine($"c1 = {c1}");
+        Console.WriteLine(c1 == c2);
+        Console.WriteLine(c1 == c3);
+
+        var p1 = new Point3D(1, 2, 3, "black");
+        var p2 = new Point3D(1, 2, 3, "black");
+        var p3 = p1;
+        Console.WriteLine($"p1 = {p1}");
+        Console.WriteLine(p1 == p2);
+        Console.WriteLine(p1 == p3);
+    }
+}
