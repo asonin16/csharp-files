23585a22375d15a7172c646636db1fb38d67518d
Alexey Sonin <asonin16@yahoo.com>
2025-10-07 16:10:16 +0300

    Create NewProject204

---
+++ CSharp-L1/NewProject204/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace NewProject204;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var random = new Random();
+            int n = random.Next(-10, 10);
+            if (n > 0)
+            {
+                Console.WriteLine($"{n} > 0");
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine($"{n} < 0");
+            }
+            else
+            {
+                Console.WriteLine($"{n} = 0");
+            }
+        }
+
+        string? name = null;
+        Console.WriteLine(name?.ToUpper());
+
+        string? text1 = name ?? "no name";
+        name ??= "no name";
+        string? text2 = null;
+        _ = text2 ?? "no name";
+        Console.WriteLine(name);
+        Console.WriteLine(text1);
+        Console.WriteLine(text2);
+    }
+}
