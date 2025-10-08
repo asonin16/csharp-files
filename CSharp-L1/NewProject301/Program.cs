9b8fa0275388a774532e057a9856e955c1743226
Alexey Sonin <asonin16@yahoo.com>
2025-10-08 10:53:57 +0300

    Update NewProject301

--- CSharp-L1/NewProject301/Program.cs
+++ CSharp-L1/NewProject301/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"i = {i}");
+        }
+        Console.WriteLine();
+
+        for (int i = 1; i < 10; i++)
+        {
+            for (int j = 1; j < 10; j++)
+            {
+                Console.Write("{0, -4}", i * j);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        int a = 2;
+
+        while (a < 1000)
+        {
+            Console.WriteLine(a);
+            a *= 2;
+        }
+        Console.WriteLine();
+
+        a = 2;
+        do
+        {
+            Console.WriteLine(a);
+            a *= 2;
+        } while (a < 1000);
+        Console.WriteLine();
     }
 }
