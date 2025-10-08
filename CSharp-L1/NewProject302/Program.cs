dc3a02dd1c95c786f4d720464ae9cf0d4413413b
Alexey Sonin <asonin16@yahoo.com>
2025-10-08 12:56:05 +0300

    Update NewProject302

--- CSharp-L1/NewProject302/Program.cs
+++ CSharp-L1/NewProject302/Program.cs
@@ -18,7 +18,18 @@ class Program
     {
         return (x + y) / 2.0;
     }
+    public static double Average(int x, int y, int z) => (x + y + z) / 3.0;
     public static double Average(double x, double y) => (x + y) / 2.0;
+    public static double Average(params int[] arr)
+    {
+        long sum = 0L;
+        foreach (int k in arr)
+            sum += k;
+        return (double)sum / arr.LongLength;
+    }
+    public static int Increment(ref int input) => ++input;
+    public static void TestMethod(in int a, out int b) => b = a * a;
+
     static void Main(string[] args)
     {
         var p = new Program();
@@ -30,5 +41,14 @@ class Program
         SayHello3(age: 20);
 
         Console.WriteLine(Average(12, 45));
+        Console.WriteLine(Average(12.67, 45.98));
+        Console.WriteLine(Average(24, 43, 89));
+        Console.WriteLine(Average(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+        int value = 23;
+        Console.WriteLine(Increment(ref value));
+
+        TestMethod(in value, out int result);
+        Console.WriteLine(result);
     }
 }
