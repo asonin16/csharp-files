3270935423d1c045e8612033f36193ee380b11e9
Alexey Sonin <asonin16@yahoo.com>
2025-10-09 12:50:53 +0300

    Update NewProject401

--- CSharp-L1/NewProject401/Course.cs
+++ CSharp-L1/NewProject401/Course.cs
@@ -2,31 +2,77 @@ namespace NewProject401;
 
 public class Course
 {
-    public string? Title;
-    public int Length;
+    // public string? Title;
+    // private string title;
+
+    // public string Title
+    // {
+    //     get { return title; }
+    //     set { title = value; }
+    // }
+
+    public string Title { get; set; } = "New course";
+
+    public string Description { get; }  // Read-only property
+
+    private int length2;
+
+    public const int MIN_LENGTH = 8;
+    public const int MAX_LENGTH = 48;
+
+    // public int GetLength()
+    // {
+    //     return Length;
+    // }
+
+    // public void SetLength(int Length)
+    // {
+    //     if (Length < MIN_LENGTH || Length > MAX_LENGTH)
+    //         throw new ArgumentException("Lenght is outside of range");
+    //     this.Length = Length;
+    // }
+
+    // Property:
+    public int Length2
+    {
+        get { return length2; }
+        set
+        {
+            if (value < MIN_LENGTH || value > MAX_LENGTH)
+                throw new ArgumentException("Length is outside of range");
+            length2 = value;
+        }
+    }
+
     static int Counter = 0;
+
     public static void PrintCounter()
     {
         // "this" variable is not available here (static) directly
         Console.WriteLine($"Course counter: {Counter}");
     }
+
     static Course()
     {
         // it is called when the class is invoked for a first time
     }
-    public Course() : this("New course", 8)
+
+    public Course() : this("New course", "", 8)
     {
         Console.WriteLine("#Course() stop#");
     }
-    public Course(string Title, int Length = 12)
+
+    public Course(string Title, string Description, int Length2 = 12)
     {
         this.Title = Title;
-        this.Length = Length;
+        this.Description = Description;
+        this.Length2 = Length2;
         Counter++;
     }
+
     public void Show()
     {
-        Console.WriteLine($"{Title} : {Length}");
+        Console.WriteLine($"{Title} : {Description} : {Length2}");
     }
 
 }
--- CSharp-L1/NewProject401/Program.cs
+++ CSharp-L1/NewProject401/Program.cs
@@ -16,15 +16,20 @@ internal class Program
 
         Course.PrintCounter();
 
-        var c3 = new Course("C++ Language", 40);
-        var c4 = new Course("Rust language");
+        var c3 = new Course("C++ Language", "Short introduction", 40);
+        var c4 = new Course("Rust language", "Short introduction");
         var c5 = new Course();
+        c5.Title = "Ruby language";
+        // c5.Description = "Basic tutorial";
+        c5.Length2 = 16;
+        var c6 = new Course("Python Language", "Short introduction", 48);
 
         // c1.Show();
         // c2.Show();
         c3.Show();
         c4.Show();
         c5.Show();
+        c6.Show();
 
         Course.PrintCounter();
     }
