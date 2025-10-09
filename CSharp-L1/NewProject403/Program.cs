8b74c1aeb6fd850424c133460f1b9d96a767bd51
Alexey Sonin <asonin16@yahoo.com>
2025-10-09 16:56:35 +0300

    Update NewProject403

--- CSharp-L1/NewProject403/Program.cs
+++ CSharp-L1/NewProject403/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Anonymous class / object
+        var book = new { Title = "Title", Author = "Author", Price = 212.95M };
+        Console.WriteLine(book);
+        Console.WriteLine(book.Title);
+        Console.WriteLine(book.Author);
+        Console.WriteLine(book.Price);
     }
 }
