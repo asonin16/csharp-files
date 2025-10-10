c060d31b3866f48f90b2591b9e70989d9790fb68
Alexey Sonin <asonin16@yahoo.com>
2025-10-10 11:51:24 +0300

    Update NewProject501

--- CSharp-L1/NewProject501/Program.cs
+++ CSharp-L1/NewProject501/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace NewProject501;
+﻿using System.Reflection;
+
+namespace NewProject501;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var obj = new object();
+        var type = obj.GetType();
+
+        // MethodInfo mi = type.GetMethod("Name");
+        // PropertyInfo pi = type.GetProperty("Show");
+
+        // Without reflection
+        Program foo = new();
+        foo.PrintHello();
+
+        // With reflection
+        // Object foo1 = Activator.CreateInstance("complete.classpath.and.Program");
+        // MethodInfo method = foo1.GetType().GetMethod("PrintHello");
+        // method.Invoke(foo1, null);
+
+    }
+
+    public void PrintHello()
+    {
+        Console.WriteLine("Hello, world!");
     }
 }
