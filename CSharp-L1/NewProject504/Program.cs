ac81d7fbc33e87e67405fb5dc4598489ee88db54
Alexey Sonin <asonin16@yahoo.com>
2025-10-10 15:03:48 +0300

    Create NewProject504

---
+++ CSharp-L1/NewProject504/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace NewProject504;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(GC.MaxGeneration);
+        Console.WriteLine(GC.CollectionCount(0));
+        Console.WriteLine(GC.GetTotalMemory(false));
+        Console.WriteLine(GC.GetTotalAllocatedBytes());
+
+        // GC.Collect(GC.MaxGeneration);
+        // GC.WaitForFullGCComplete();
+
+        // GC.AddMemoryPressure(10000);
+        // // can be used for unsafe code
+        // GC.RemoveMemoryPressure(10000);
+
+        // var obj = new Object();
+        // GC.SuppressFinalize(obj);
+        // GC.ReRegisterForFinalize(obj);
+
+        // ### OPTION 1 ###
+        DBConnection dbc = new DBConnection();
+        try
+        {
+            dbc.Execute();
+        }
+        finally
+        {
+            dbc.Dispose();
+        }
+
+        // ### OPTION 2 ###
+        using (DBConnection conn = new DBConnection())
+        {
+            conn.Execute();
+            //conn.Dispose();
+        }
+
+        // ### OPTION 3 ###
+        using DBConnection dcon = new DBConnection();
+        
+    }
+
+    public class DBConnection : IDisposable
+    {
+        public DBConnection() => Console.WriteLine("Open connection");
+        public void Execute() => Console.WriteLine("Execute command");
+        public void Dispose() => Console.WriteLine("Close connection");
+        
+        // public virtual void Dispose(bool disposing)
+        // {
+        //     if (disposing)
+        //     {
+        //         // TODO
+        //         Console.WriteLine("Close connection");
+        //     }
+        // }
+
+    }
+}
