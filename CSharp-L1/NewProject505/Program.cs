e5ec75f49e3702b73ef95830b21d8ecbdf9232f0
Alexey Sonin <asonin16@yahoo.com>
2025-10-10 16:08:11 +0300

    Create NewProject505

---
+++ CSharp-L1/NewProject505/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace NewProject505;
+
+class Program
+{
+    const string fileName = "testfile.txt";
+    const string dirName = "";
+    const string pathName = @"C:\Users\asoni\Documents\csharp-files\CSharp-L1\NewProject505\bin\Debug\net8.0\testfile.txt";
+
+    static void Main(string[] args)
+    {
+        if (!File.Exists(fileName))
+        {
+            File.Create(fileName);
+        }
+
+        string content = File.ReadAllText(fileName);
+
+        Console.WriteLine(content);
+
+        FileStream fs = File.OpenRead(pathName);
+        
+    }
+}
