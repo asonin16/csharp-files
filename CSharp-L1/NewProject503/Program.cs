ba66be8a05a29ec16243f88aab55581a097d475a
Alexey Sonin <asonin16@yahoo.com>
2025-10-10 13:06:24 +0300

    Create NewProject503

---
+++ CSharp-L1/NewProject503/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace NewProject503;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+// Put each interface in separate file
+
+public interface IWork
+{
+    void DoWork(); // public by default
+}
+
+public interface IScaleable
+{
+    void Scale(double factor);
+}
