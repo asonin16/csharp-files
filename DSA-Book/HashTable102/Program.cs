ce34017c5ff715b30a1e9a37a2a17c028213b94b
Alexey Sonin <asonin16@yahoo.com>
2025-10-12 14:31:28 +0300

    Create HashTable102

---
+++ DSA-Book/HashTable102/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+
+namespace HashTable102;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Hashtable phoneBook = new()
+        {
+            { "Marcin", "101-202-303" },
+            { "John", "202-303-404" }
+        };
+        phoneBook["Aline"] = "303-404-505";
+        phoneBook["Maria"] = "404-505-606";
+        phoneBook["Aline"] = "505-606-707";
+
+        Console.WriteLine("Phone numbers:");
+        if (phoneBook.Count == 0)
+        {
+            Console.WriteLine("Empty list.");
+        }
+        foreach (DictionaryEntry entry in phoneBook)
+        {
+            Console.WriteLine($"{entry.Key}: {entry.Value}");
+        }
+
+        Console.Write("\nSearch by name: ");
+        string name = Console.ReadLine() ?? string.Empty;
+        if (phoneBook.ContainsKey(name))
+        {
+            string number = (string)phoneBook[name]!;
+            Console.WriteLine($"Phone number: {number}");
+        }
+        else
+        {
+            Console.WriteLine("Does not exist.");
+        }
+    }
+}
