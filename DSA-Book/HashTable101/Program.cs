eded9ff34c8916b56bee1abdaebe1d2408c3e1ce
Alexey Sonin <asonin16@yahoo.com>
2025-10-12 13:33:13 +0300

    Create HashTable101

---
+++ DSA-Book/HashTable101/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace HashTable101;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Hashtable ht1 = new()
+        {
+            { "Key1", "Val1"},
+            { "Key2", "Val2"}
+        };
+        ht1.Add("Key3", "Val3");
+        ht1["Key4"] = "Val4";
+
+        // var value = (string?)ht1["Key1"];
+
+        foreach (DictionaryEntry entry in ht1)
+        {
+            Console.WriteLine($"{entry.Key}: {entry.Value}");
+        }
+    }
+}
