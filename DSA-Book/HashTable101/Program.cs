7aa99d842d306e3d9f761f8a647f9fc6621e9139
Alexey Sonin <asonin16@yahoo.com>
2025-10-12 14:23:26 +0300

    Update HashTable101

--- DSA-Book/HashTable101/Program.cs
+++ DSA-Book/HashTable101/Program.cs
@@ -6,19 +6,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Hashtable ht1 = new()
+        /* Hashtable methods:
+            Add, Remove, Clear, Clone,
+            Contains, ContainsKey, ContainsValue,
+            Equals, GetHashCode, GetType...
+            ...and properties:
+            Count, Keys, Values
+        */
+
+        Hashtable hashtable = new()
         {
             { "Key1", "Val1"},
             { "Key2", "Val2"}
         };
-        ht1.Add("Key3", "Val3");
-        ht1["Key4"] = "Val4";
+        hashtable.Add("Key3", "Val3");
+        hashtable["Key4"] = "Val4";
+        hashtable["Key5"] = "Val5";
 
-        // var value = (string?)ht1["Key1"];
+        // var value = (string?)hashtable["Key1"];
 
-        foreach (DictionaryEntry entry in ht1)
+        foreach (DictionaryEntry entry in hashtable)
         {
             Console.WriteLine($"{entry.Key}: {entry.Value}");
+        }
+
+        Console.WriteLine($"Count: {hashtable.Count}");
+
+        var keys = hashtable.Keys;
+        var values = hashtable.Values;
+
+        foreach (string key in keys)
+        {
+            Console.WriteLine(key);
+        }
+
+        foreach (string value in values)
+        {
+            Console.WriteLine(value);
         }
     }
 }
