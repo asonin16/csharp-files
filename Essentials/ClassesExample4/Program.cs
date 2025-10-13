5b59605c98c5fbd652e3ac7693714da1896b620a
Alexey Sonin <asonin16@yahoo.com>
2025-10-13 16:34:10 +0300

    Update ClassesExample4

--- Essentials/ClassesExample4/Program.cs
+++ Essentials/ClassesExample4/Program.cs
@@ -9,7 +9,18 @@ internal class Program
         {
             Salary = "Too Little"
         };
-
         Console.WriteLine("{0} {1}: {2}", employee.FirstName, employee.LastName, employee.Salary);
+
+        List<Employee> employees = new()
+        {
+            new("John", "Donovan"),
+            new("Miles", "Davis"),
+            new("Thomas", "Black")
+        };
+
+        foreach (var emp in employees)
+        {
+            Console.WriteLine($"{emp.FirstName} {emp.LastName}: {emp.Salary}");
+        }
     }
 }
