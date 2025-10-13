e610b5ca9d163452f135aca5e523003d6e994b58
Alexey Sonin <asonin16@yahoo.com>
2025-10-13 16:08:06 +0300

    Update ClassesExample3

--- Essentials/ClassesExample3/Program.cs
+++ Essentials/ClassesExample3/Program.cs
@@ -4,10 +4,11 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Employee employee = new("Inigo", "Montoya")
-        {
-            Salary = "Too Little"
-        };
+        Employee employee = new("Inigo", "Montoya");
+        employee.Salary = "Too Little";
+        // {
+        //     Salary = "Too Little"
+        // };
 
         Console.WriteLine($"{employee.FirstName} {employee.LastName}: {employee.Salary}");
     }
