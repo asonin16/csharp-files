71db516880dfa9ad264e3d5fca2ae25de73a21b6
Alexey Sonin <asonin16@yahoo.com>
2025-10-13 15:47:16 +0300

    Create ClassesExample3

---
+++ Essentials/ClassesExample3/Employee.cs
@@ -0,0 +1,34 @@
+namespace ClassesExample3;
+
+// Employee constructor
+public class Employee(string firstName, string lastName)
+{
+    public string FirstName { get; set; } = firstName;
+    public string LastName { get; set; } = lastName;
+    public string? Salary { get; set; } = "Not Enough";
+    public string? Title { get; set; }
+    public Employee? Manager { get; set; }
+
+    // Name property
+    public string FullName
+    {
+        get { return FirstName + " " + LastName; }
+        set
+        {
+            // Split the assigned value into 
+            // first and last names
+            string[] names = value.Split(' ');
+            if (names.Length == 2)
+            {
+                FirstName = names[0];
+                LastName = names[1];
+            }
+            else
+            {
+                // Throw an exception if the full 
+                // name was not assigned
+                throw new ArgumentException($"Assigned value '{value}' is invalid", nameof(value));
+            }
+        }
+    }
+}
---
+++ Essentials/ClassesExample3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassesExample3;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Employee employee = new("Inigo", "Montoya")
+        {
+            Salary = "Too Little"
+        };
+
+        Console.WriteLine($"{employee.FirstName} {employee.LastName}: {employee.Salary}");
+    }
+}
