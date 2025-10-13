c1250c586deb8ddc7613ab793fb18cded118ea00
Alexey Sonin <asonin16@yahoo.com>
2025-10-13 19:57:02 +0300

    Create ClassesExample6

---
+++ Essentials/ClassesExample6/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassesExample6;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Employee employee1 = new("Inigo", "Montoya");
+        Employee employee2 = new(42);
+        employee2.FirstName = "John Doe";
+
+        Console.WriteLine($"{employee1.Id} - {employee1.FullName}: {employee1.Salary}");
+        Console.WriteLine($"{employee2.Id} - {employee2.FullName}: {employee2.Salary}");
+    }
+}
