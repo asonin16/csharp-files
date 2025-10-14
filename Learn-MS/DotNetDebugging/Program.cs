d10b10e6794735c54210f2cdc26b188ee3e6b7ab
Alexey Sonin <asonin16@yahoo.com>
2025-10-14 19:51:48 +0300

    Update DotNetDebugging

--- Learn-MS/DotNetDebugging/Program.cs
+++ Learn-MS/DotNetDebugging/Program.cs
@@ -1,15 +1,39 @@
-﻿namespace DotNetDebugging;
+﻿using System.Diagnostics;
+
+namespace DotNetDebugging;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        int result = Fibonacci(5);
+        int result = Fibonacci(6);
         Console.WriteLine(result);
+
+        // Console.WriteLine("This message is readable by the end user.");
+        // Trace.WriteLine("This is a trace message when tracing the app.");
+        // Debug.WriteLine("This is a debug message just for developers.");
+
+        // Debug.Write("Debug - ");
+        // Debug.WriteLine("This is a full line.");
+        // Debug.WriteLine("This is another full line.");
+
+        // int count = 0;
+        // Debug.WriteLineIf(count == 0, "The count is 0 and this may cause an exception.");
+
+        // bool errorFlag = false;  
+        // Trace.WriteIf(errorFlag, "Error in AppendData procedure.");  
+        // Debug.WriteIf(errorFlag, "Transaction abandoned.");
+        // Trace.Write("Invalid value for data request");
+
+        // Console.WriteLine(IntegerDivide(2, 2));
+        // Console.WriteLine(IntegerDivide(2, 0));
     }
 
     static int Fibonacci(int n)
     {
+        Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+        Debug.WriteLine($"We are looking for the {n}th number");
+
         int n1 = 0;
         int n2 = 1;
         int sum;
@@ -19,8 +43,20 @@ internal class Program
             sum = n1 + n2;
             n1 = n2;
             n2 = sum;
+            Debug.WriteLineIf(sum == 1, $"sum is 1, n1 is {n1}, n2 is {n2}");
         }
 
+        // If n2 is 5 continue, else break.
+        Debug.Assert(n2 == 5, "The return value is not 5 and it should be.");
         return n == 0 ? n1 : n2;
     }
+
+/*
+    static int IntegerDivide(int dividend, int divisor)
+    {
+        Debug.Assert(divisor != 0, $"{nameof(divisor)} is 0 and will cause an exception.");
+
+        return dividend / divisor;
+    }
+*/
 }
