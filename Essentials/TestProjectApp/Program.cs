1e441dc22366dd12107bbcd595e95d9f418d9d21
Alexey Sonin <asonin16@yahoo.com>
2025-10-14 12:35:13 +0300

    Update TestProjectApp

--- Essentials/TestProjectApp/Program.cs
+++ Essentials/TestProjectApp/Program.cs
@@ -8,33 +8,43 @@ class Program
     {
         var (name, capital, population, gdpPerCapita) = ("Russia", "Moscow", 142_000_000.00M, 13000.00M);
         Console.WriteLine($"Country: {name},\nCapital: {capital},\nPopulation: {population},\nGDP per capita: {gdpPerCapita}.");
-        
+
         char[] ch = ['A', 'B', 'C', 'D', 'E', 'F'];
         Console.WriteLine(new string(ch[..]));
         Console.WriteLine(string.Join("", ch[..].Reverse()));
 
-        string s = "Hello, CSharp";
-
-        IEnumerator rator = s.GetEnumerator();
+        string s1 = "First sample";
+        IEnumerator rator = s1.GetEnumerator();
         while (rator.MoveNext())
         {
             char c = (char)rator.Current;
             Console.Write(c + ".");
         }
-
         Console.WriteLine();
 
-        foreach (char c in s)
+        string s2 = "Second one";
+        foreach (char c in s2)
+        {
             Console.Write(c + ".");
-
+        }
         Console.WriteLine();
 
-        string[] names = { "Tom", "Dick", "Harry", "Mary", "Jay" };
+        string[] names = ["Tom", "Dick", "Harry", "Mary", "Jay"];
         IEnumerable<string> query = names
-            .Where (n => n.Contains('a'))
-            .OrderBy (n => n.Length)
-            .Select (n => n.ToUpper());
+            .Where(n => n.Contains('a'))
+            .OrderBy(n => n.Length)
+            .Select(n => n.ToUpper());
         foreach (string name2 in query)
-            Console.WriteLine (name2);
+        {
+            Console.WriteLine(name2);
+        }
+
+        HashSet<string> set1 = ["one", "two", "three", "four", "five"];
+        HashSet<string> set2 = ["four", "five", "six", "seven", "eight"];
+        set1.UnionWith(set2);
+        foreach (string str in set1)
+        {
+             Console.WriteLine(str);
+        }
     }
 }
