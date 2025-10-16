c3c19b142d1f9e5326ed485287b68f8a39dca995
Alexey Sonin <asonin16@yahoo.com>
2025-10-16 14:53:49 +0300

    Create Collections102

---
+++ SProjects/Collections102/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Collections102;
+
+internal class Program
+{
+    static void Main()
+    {
+        // Example: Managing a list of students
+
+        var students = new List<Student>
+        {
+            new Student { Name = "Haneul", Age = 20 },
+            new Student { Name = "Magda", Age = 22 }
+        };
+
+        students.Add(new Student { Name = "Dale", Age = 23 });
+        students.RemoveAt(0); // Remove Haneul
+
+        foreach (Student student in students)
+        {
+            Console.WriteLine($"Name: {student.Name}, Age: {student.Age}");
+        }
+
+        // Expected output:
+        // Name: Magda, Age: 22
+        // Name: Dale, Age: 23
+
+    }
+}
---
+++ SProjects/Collections102/Student.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Collections102;
+
+public class Student
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
