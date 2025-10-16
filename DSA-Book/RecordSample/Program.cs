6f5f27094afe43a0b2f3cfe57429c53b7b0f0035
Alexey Sonin <asonin16@yahoo.com>
2025-10-16 20:32:48 +0300

    Create RecordSample

---
+++ DSA-Book/RecordSample/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace RecordSample;
+
+public record Dog(string Name, string Breed, int Height, float Weight, int Age);
+
+internal class Program
+{
+    static void Main()
+    {
+        var rex = new Dog("Rex", "Schnauzer", 40, 11, 5);
+        Console.WriteLine(rex);
+
+        var beauty = rex with { Name = "Beauty", Height = 35 };
+        Console.WriteLine(beauty);
+        
+        (string name, _, _, _, int age) = beauty;
+        Console.WriteLine($"Name = {name}, Age = {age}");
+    }
+}
