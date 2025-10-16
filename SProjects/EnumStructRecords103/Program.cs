80a93bcdb88b23122b384a1aa09de52dd68f0bc5
Alexey Sonin <asonin16@yahoo.com>
2025-10-16 20:24:18 +0300

    Create EnumStructRecords103

---
+++ SProjects/EnumStructRecords103/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EnumStructRecords103;
+
+public record Dog(string Name, string Breed, int Height, float Weight, int Age);
+
+internal class Program
+{
+    static void Main()
+    {
+        var rex = new Dog("Rex", "Schnauzer", 40, 11, 5);
+        Console.WriteLine(rex);
+
+        var beauty = rex with { Name = "Beauty", Height = 35 };
+        Console.WriteLine(beauty);
+        (string name, _, _, _, int age) = beauty;
+        Console.WriteLine($"Name = {name}, Age = {age}");
+    }
+}
