c86d268cef8dbdabe24621839ee69361f080d6bd
Alexey Sonin <asonin16@yahoo.com>
2025-10-17 16:36:43 +0300

    Create GenericsAnonymous104

---
+++ SProjects/GenericsAnonymous104/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace GenericsAnonymous104;
+
+internal class Program
+{
+    static void Main()
+    {
+        var v = new { Amount = 108, Message = "Hello" };
+        Console.WriteLine($"{v.Amount} - {v.Message}");
+
+        var p = new { Name = "Laptop", Price = 1200 };
+        Console.WriteLine($"Product: {p.Name}, Price: {p.Price}");
+
+        var products = new[] {
+            new { Name = "Laptop", Price = 1200 },
+            new { Name = "Tablet", Price = 600 },
+            new { Name = "Display", Price = 400 },
+            new { Name = "Printer", Price = 700 },
+            new { Name = "Smartphone", Price = 1500 }
+        };
+
+        var filteredProducts = from pr in products
+                            where pr.Price < 1000
+                            select new { pr.Name, pr.Price };
+
+        foreach (var product in filteredProducts)
+        {
+            Console.WriteLine($"Name: {product.Name}, Price: {product.Price}");
+        }
+
+        var customer = new { Name = "Mario Rogers", Age = 30 };
+        Console.WriteLine($"Customer: {customer.Name}, Age: {customer.Age}");
+
+    }
+}
