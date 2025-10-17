8fac69d3055a1ab7ff7931d1dedc0908a6b1553a
Alexey Sonin <asonin16@yahoo.com>
2025-10-17 14:53:43 +0300

    Create GenericsAnonymous102

---
+++ SProjects/GenericsAnonymous101/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace GenericsAnonymous101;
+
+public class Box<T>
+{
+    public T? Item { get; set; }
+
+    public void AddItem(T item)
+    {
+        Item = item;
+    }
+}
+internal class Program
+{
+    static void Main()
+    {
+        List<int> numbers = [1, 2, 3];
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+        var names = new List<string> { "Hannah", "Mario" };
+        string firstName = GetFirstItem(names);
+        Console.WriteLine(firstName);
+    }
+
+    public static T GetFirstItem<T>(List<T> items)
+    {
+        return items[0];
+    }
+}
