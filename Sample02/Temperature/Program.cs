2146e4961bbcd8dc7502762f2e06c951dc6feb8c
Alexey Sonin <asonin16@yahoo.com>
2025-10-17 18:12:03 +0300

    Create Temperature

---
+++ Sample02/Temperature/Program.cs
@@ -0,0 +1,48 @@
+﻿[assembly: CLSCompliant(true)]
+
+namespace Temperature;
+
+public interface IFahrenheit
+{
+    decimal GetTemperature();
+}
+
+public interface ICelsius
+{
+    decimal GetTemperature();
+}
+
+public class Temperature : ICelsius, IFahrenheit
+{
+    private readonly decimal _value;
+
+    public Temperature(decimal value)
+    {
+        // We assume that this is the Celsius value.
+        _value = value;
+    }
+
+    decimal IFahrenheit.GetTemperature()
+    {
+        return _value * 9 / 5 + 32;
+    }
+
+    decimal ICelsius.GetTemperature()
+    {
+        return _value;
+    }
+}
+public class Example
+{
+    public static void Main()
+    {
+        var temp = new Temperature(100.0m);
+        ICelsius cTemp = temp;
+        IFahrenheit fTemp = temp;
+        Console.WriteLine($"Temperature in Celsius: {cTemp.GetTemperature()} degrees");
+        Console.WriteLine($"Temperature in Fahrenheit: {fTemp.GetTemperature()} degrees");
+    }
+}
+// The example displays the following output:
+//       Temperature in Celsius: 100.0 degrees
+//       Temperature in Fahrenheit: 212.0 degrees
